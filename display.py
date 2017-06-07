3e3a50c0407c067b97c2e33c38b5e0de82486dc5
ism0080 <isaac.mackle@gmail.com>
2017-06-07 22:27:50 +1200

    Final Commits

--- display.py
+++ display.py
@@ -4,19 +4,6 @@ http://www.pygal.org/en/latest/documentation/index.html
 import pygal
 from abc import abstractmethod, ABCMeta
 
-
-# class PyGal(object):
-#     def bar_char(self, id, dictionary):
-#         try:
-#             bar_chart = pygal.Bar(title='Employee Information', x_title='Employee ID\'s', y_title='Employee Data')
-#             for key in dictionary:
-#                 bar_chart.add(key, dictionary[key])  # Add some values
-#             bar_chart.x_labels = id
-#             # bar_chart.render_in_browser()
-#             return True
-#         except Exception as err:
-#             print("The exception is: Invalid Data", err)
-
 class AbstractBuilder(metaclass=ABCMeta):
     def __init__(self):
         self._chart = None
--- reader.py
+++ reader.py
@@ -1,37 +1,6 @@
 from abc import ABCMeta, abstractmethod
 import csv
 
-# class FileReader(object):
-#     def __init__(self):
-#         self.line = []
-#         self.list_of_dictionaries = []
-#
-#     def read(self, filename):
-#         try:
-#             with open(filename, 'r') as file:
-#                 file_data = file.read()
-#                 new_line_split = file_data.split('\n')
-#
-#                 self.splits_on_comma_separation(new_line_split)
-#                 self.create_list_of_dictionaries()
-#
-#             return self.list_of_dictionaries
-#         except IOError as err:
-#             print("The exception is: ", err)
-#             pass
-#
-#     def create_list_of_dictionaries(self):
-#         for empty in self.list_of_dictionaries:
-#             self.list_of_dictionaries[empty] \
-#                 = dict(e.split('=') for e in self.line[empty])
-#
-#     def splits_on_comma_separation(self, new_line_split):
-#         i = 0
-#         for x in new_line_split:
-#             self.line.extend([new_line_split[i].split(', ')])
-#             self.list_of_dictionaries.extend([i])
-#             i += 1
-
 
 class FileReader(object):
     def __init__(self, file_type):
